4061efcb98b1a31f8041dea5ada0d969960d125a
Soheil Nazari <kontakt@soheilnazari.de>
2025-12-07 20:56:38 +0100

    add nitro as server framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { useEffect, useState } from 'react'
+
 export default function App() {
-  return <div>Vite app</div>
+  const [data, setData] = useState<string | null>(null)
+
+  useEffect(() => {
+    fetch('/api/dummy')
+      .then((r) => r.text())
+      .then((r) => setData(r))
+  }, [])
+
+  return <>{data ? <div dangerouslySetInnerHTML={{ __html: data }}></div> : <div>vite app</div>}</>
 }
