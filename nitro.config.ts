0546f9ac89c83cc747f06434f84db75292cfc2ed
Soheil Nazari <kontakt@soheilnazari.de>
2025-12-15 18:17:59 +0100

    split into app, server and shared dirs

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,6 +3,6 @@ import { defineNitroConfig } from 'nitropack/config'
 // https://nitro.build/config
 export default defineNitroConfig({
   compatibilityDate: 'latest',
-  srcDir: 'src',
+  srcDir: 'server',
   imports: false,
 })
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: `${import.meta.dirname}/app`,
+  build: {
+    outDir: `${import.meta.dirname}/dist`,
+  },
   plugins: [
     react({
       babel: {
