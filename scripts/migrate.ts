09e692940dfa27fe20c234ec33d7387e35d594cf
Soheil Nazari <kontakt@soheilnazari.de>
2026-05-25 23:00:21 +0200

    run fmt, update docker postgres volume

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -1,5 +1,5 @@
-import {migrate} from 'drizzle-orm/node-postgres/migrator'
-import {connection, db} from '../server/infrastructure/Database/client.ts'
+import { migrate } from 'drizzle-orm/node-postgres/migrator'
+import { connection, db } from '../server/infrastructure/Database/client.ts'
 
 await migrate(db, {
   migrationsFolder: './server/infrastructure/Database/migrations',
